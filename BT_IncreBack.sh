bed95ad02892cd4b55b4b4397cab0a280aa20397
Minpyo(Min) Kim <pyo970120@gmail.com>
2021-06-25 19:27:52 -0500

    Timestamp update
    Clean up some code
    Also, changed to temperate snapshot to use the same timestamp with the backup side

--- BT_IncreBack.sh
+++ BT_IncreBack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
-#===============================================================================
-# Cheking privilege ------------------------------------------------------------
+##==============================================================================
+## Cheking privilege -----------------------------------------------------------
 # Scrip requried to run on rooot, or sudo user
 if [[ $EUID -ne 0 ]]; then
     echo
@@ -16,18 +16,17 @@ if [[ $EUID -ne 0 ]]; then
     exit 2
 fi
 
-#===============================================================================
-# Basic settings ---------------------------------------------------------------
+##==============================================================================
+## Basic settings --------------------------------------------------------------
 BT_ROOT_D=/mnt/DATA_SSD/                    # Root Mount point of souece
 BT_ROOT_B=/mnt/Backup/                      # Root Mount point of backup
 SRCPATH=/mnt/DATA_SSD                       # Root of source dir
 SNAP_PATH_T=/mnt/DATA_SSD/.snapshot         # Temperate dir for snapshot
 SNAP_PATH_I=/mnt/Backup/incremental         # Backup destination
 SNAP_PATERN=@GMT-$(date +%Y.%m.%d_%H.%M.%S) # Get current time
-LOG_FILE=$SNAP_PATH_I/log                   # Directory for log
 
-#===============================================================================
-# Backup star ------------------------------------------------------------------
+##==============================================================================
+## Backup star -----------------------------------------------------------------
 # List fo source directories
 declare -a \
     DIR_LIST=(
@@ -48,7 +47,7 @@ if [ "$(date +%H.%M)" == "05.00" ]; then
 
     # Find and delect snapshot more then month for each directory
     for DIR in ${DIR_LIST[@]}; do
-        $SN_Del -s $SNAP_PATH_I/@SN_$DIR >>$LOG_FILE/Del-SN_$DIR.log
+        $SN_Del -s $SNAP_PATH_I/@SN_$DIR
         # Force a sync on a filesystem
         btrfs filesystem sync $BT_ROOT_B
     done
@@ -56,33 +55,26 @@ if [ "$(date +%H.%M)" == "05.00" ]; then
 fi
 
 for DIR in ${DIR_LIST[@]}; do
-    tmp_sn=$SNAP_PATH_T/@SN_$DIR/@$DIR    # Full tmp sn dir
-    tmo_o=$SNAP_PATH_T/@SN_$DIR/@old_$DIR # Full tmp old sn dir
-    des=$SNAP_PATH_I/@SN_$DIR             # Full destination dir
-
+    tmp_sn=$SNAP_PATH_T/@SN_$DIR # Full tmp sn dir
+    tmp_o=$(ls -A $tmp_sn/)      # Get old snapshot
+    des=$SNAP_PATH_I/@SN_$DIR    # Full directory of destination
     # Creating temperate snapshot before send to backup drive
-    btrfs subvolume snapshot -r $SRCPATH/@$DIR $tmp_sn >>$LOG_FILE/TSN_$DIR.log
+    btrfs subvolume snapshot -r $SRCPATH/@$DIR $tmp_sn/$SNAP_PATERN
     # Force a sync on a filesystem
     btrfs filesystem sync $BT_ROOT_D
-
-    #Send only difference if there is old sanp exists, else send initial copy
-    if [ -d "$tmo_o" ]; then
-        btrfs send -p $tmo_o $tmp_sn |
-            btrfs receive -v $des >>$LOG_FILE/IRSN_$DIR.log
-        btrfs subvolume delete -v $tmo_o >>$LOG_FILE/TSN_$DIR.log
+    # If directory is empty, send intial snapshot
+    if [[ -z $tmp_o ]]; then
+        btrfs send $tmp_sn/$SNAP_PATERN | btrfs receive $des
     else
-        btrfs send $tmp_sn |
-            btrfs receive -v $des >>$LOG_FILE/IRSN_$DIR.log
+        tmp_o=$tmp_sn/$tmp_o
+        # Send only difference
+        btrfs send -p $tmp_o $tmp_sn/$SNAP_PATERN | btrfs receive $des
+        # Delect temperate snapshot
+        btrfs subvolume delete -Cv $tmp_o
     fi
-
-    # Cleanup
-    mv $tmp_sn $tmo_o
-    mv $des/@$DIR $des/$SNAP_PATERN
-
     # Force a sync on a filesystem
     btrfs filesystem sync $BT_ROOT_D
     btrfs filesystem sync $BT_ROOT_B
 done
-
 # Exit scrip
 exit 0
--- tmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-declare -a \
-    DIR_LIST=(
-        /nfs/DATA/*
-    )
-echo ${DIR_LIST[@]}
