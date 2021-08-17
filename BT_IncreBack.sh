7ebd5a3a6ba2e4e6e7315a0c06d5d5d3ac7f97ee
Minpyo Kim <pyo970120@gmail.com>
2021-08-16 19:43:09 -0500

    Some updates
    BT_IncreBack.sh
    - Add output message and fix for some bugs
    
    snap_del
    -re-compiled to reflect btrfs_auto_snap_delete (https://github.com/LinearAlpha/btrfs_auto_snap_delete)

--- BT_IncreBack.sh
+++ BT_IncreBack.sh
@@ -23,10 +23,9 @@ BT_ROOT_B=/mnt/Backup/                      # Root Mount point of backup
 SRCPATH=/mnt/DATA_SSD                       # Root of source dir
 SNAP_PATH_T=/mnt/DATA_SSD/.snapshot         # Temperate dir for snapshot
 SNAP_PATH_I=/mnt/Backup/incremental         # Backup destination
-SNAP_PATERN=@GMT-$(date +%Y.%m.%d_%H.%M.%S) # Get current time
+TMP_TIME=$(date +%Y.%m.%d_%H.%M.%S)
+SNAP_PATERN=@CST-$TMP_TIME # Get current time
 
-##==============================================================================
-## Backup star -----------------------------------------------------------------
 # List fo source directories
 declare -a \
     DIR_LIST=(
@@ -34,30 +33,38 @@ declare -a \
         picture
     )
 
+##==============================================================================
+## Backup start-----------------------------------------------------------------
+
+# Print current time for snapshot
+printf "\n=================================================\n"
+printf "Snapshot time: $TMP_TIME===============\n"
+
 # If it is 5 am, add DATA to backup and delect old snapshots
-if [ "$(date +%H.%M)" == "05.00" ]; then
+if [ "$(date +%M)" == "00" ]; then
     DIR_LIST=(
         ${DIR_LIST[@]}
         DATA
     )
-
-    # Costom python program auto delect shanpshot
-    # Currently, this execution file is compiled on ARM based system
-    SN_Del=$(dirname "$0")/snap_del
-
-    # Find and delect snapshot more then month for each directory
-    for DIR in ${DIR_LIST[@]}; do
-        $SN_Del -s $SNAP_PATH_I/@SN_$DIR
-        # Force a sync on a filesystem
+    if [ "$(date +%H)" == "05" ]; then
+        # Costom python program auto delect shanpshot
+        # Find and delect snapshot more then month for each directory
+        for DIR in ${DIR_LIST[@]}; do
+            printf "====Delecting old sanpshot $DIR on $TMP_TIME\n"
+            $(dirname "$0")/snap_del -s $SNAP_PATH_I/@SN_$DIR
+            # Force a sync on a filesystem
+        done
+        # Synchronize cached writes to persistent storage
         btrfs filesystem sync $BT_ROOT_B
-    done
-    # Synchronize cached writes to persistent storage
+    fi
 fi
 
 for DIR in ${DIR_LIST[@]}; do
     tmp_sn=$SNAP_PATH_T/@SN_$DIR # Full tmp sn dir
     tmp_o=$(ls -A $tmp_sn/)      # Get old snapshot
     des=$SNAP_PATH_I/@SN_$DIR    # Full directory of destination
+    # Perint current working directory
+    printf "====Snapshot directory for $DIR\n"
     # Creating temperate snapshot before send to backup drive
     btrfs subvolume snapshot -r $SRCPATH/@$DIR $tmp_sn/$SNAP_PATERN
     # Force a sync on a filesystem
@@ -66,15 +73,17 @@ for DIR in ${DIR_LIST[@]}; do
     if [[ -z $tmp_o ]]; then
         btrfs send $tmp_sn/$SNAP_PATERN | btrfs receive $des
     else
-        tmp_o=$tmp_sn/$tmp_o
+        tmp_o=($tmp_sn/*)
+        tmp_o=${tmp_o[-2]}
         # Send only difference
         btrfs send -p $tmp_o $tmp_sn/$SNAP_PATERN | btrfs receive $des
         # Delect temperate snapshot
-        btrfs subvolume delete -Cv $tmp_o
+        btrfs subvolume delete $tmp_o
     fi
     # Force a sync on a filesystem
     btrfs filesystem sync $BT_ROOT_D
     btrfs filesystem sync $BT_ROOT_B
 done
+
 # Exit scrip
 exit 0
